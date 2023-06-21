dc68869a0a2a9c6fb15c7a2f3906893d688ed157
Edson Ferreira da Silva Junior <129212190+EdsonFJR@users.noreply.github.com>
2023-06-21 14:44:47 -0300

    Update teste01.py

--- teste01.py
+++ teste01.py
@@ -1,5 +1,6 @@
 nome = input('Qual é seu nome? ')
 idade = input('Qual é a sua idade? ')
 peso = input('Qual é o seu peso? ')
-print('Esses são seus dados: {},{},{} '.format(nome,idade,peso))
+local = input('Qual é o seu local de nascimento? ')
+print('Esses são seus dados: {},{},{},{} '.format(nome,idade,peso,local))
 
