48914514fba3aa290d4000bacf7d1ffe43de669d
Edson Ferreira da Silva Junior <129212190+EdsonFJR@users.noreply.github.com>
2023-06-22 00:29:42 -0300

    Update exer_003.py

--- exercicios/exer_003.py
+++ exercicios/exer_003.py
@@ -1,4 +1,4 @@
 valor1=int(input('Digite um valor:'))
 valor2=int(input('Digite outro valor:'))
 result=valor1+valor2
-print('A soma de ', valor1, '+', valor2,'= {}'.format(result))
+print('A soma de {} + {} = {}'.format(valor1, valor2, result))
